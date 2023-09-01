0472014ade41f293361f8af382855d5031679783
David Galiata | neoslash.net <david.galiata@aquia.io>
2023-09-01 10:34:05 -0700

    remove old stuff

--- components/Twemoji.js
+++
@@ -1,8 +0,0 @@
-import kebabCase from '@/lib/utils/kebabCase'
-
-const Twemoji = ({ emoji, size = 'twa-lg', className }) => {
-  const twaClass = kebabCase(emoji)
-  return <i className={`twa ${size} twa-${twaClass} ${className}`} />
-}
-
-export default Twemoji
--- data/headerNavLinks.js
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/blog', title: 'Blog' },
-  //{ href: '/snippets', title: 'Snippets' },
-  //{ href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
-  { href: '/resume', title: 'Resume' },
-]
-
-export default headerNavLinks
